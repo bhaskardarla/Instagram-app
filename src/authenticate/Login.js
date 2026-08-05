5ac53d6b9e1f9f0b61878dd2902e3e1a11763867
bhaskardarla <149381602+bhaskardarla@users.noreply.github.com>
2026-08-05 18:16:35 +0530

    Update Login.js

--- src/authenticate/Login.js
+++ src/authenticate/Login.js
@@ -12,10 +12,7 @@ function Login() {
   };
   return (
     <div className="login">
-      <img
-        src="https://www.pngkey.com/png/full/828-8286178_mackeys-work-needs-no-elaborate-presentation-or-distracting.png"
-        alt=""
-      />
+     <h1 className="app-logo">Bhaskar Insta</h1>
       <input
         onChange={(e) => setEmail(e.target.value)}
         type="email"
